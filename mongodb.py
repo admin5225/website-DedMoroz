0267695b589c83c76faac3071400e4a31677cd85
slaav1k <74726728+slaav1k@users.noreply.github.com>
2021-04-26 22:21:20 +0300

    и тут комментарии

--- mongodb.py
+++ mongodb.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient
 from tok import MONGO_DB
 from tok import MONGODB_LINK
 
-mdb = MongoClient(MONGODB_LINK)[MONGO_DB]
+mdb = MongoClient(MONGODB_LINK)[MONGO_DB]       # подключение к онлайн БД монго
 
 
-def search_or_save_user(mdb, effective_user, total):
+def search_or_save_user(mdb, effective_user, total):    # если нет пользователя - то создается, иначе поиск и возвращение
     user = mdb.users.find_one({"name": effective_user.first_name})
     if not user:
         user = {
@@ -17,7 +17,7 @@ def search_or_save_user(mdb, effective_user, total):
     return user
 
 
-def save_user_info(mdb, user, effective_user, total):
+def save_user_info(mdb, user, effective_user, total): # изменение различных аспектов объекта, в данном случае количества очков
     mdb.users.update_one(
         {'name': effective_user.first_name},
         {'$set': {'total': total}}
